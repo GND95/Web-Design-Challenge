033fbda9351a6075b80fb3807ae8c71208b0e58a
Garrett DeBlois <garrettdeblois13@yahoo.com>
2021-01-31 19:51:26 -0600

    adding python script to convert a CSV file into an HTML file. adding the generated HTML file so I can later use that HTML file as the input for a Bootstrap responsive table component

---
+++ convert_csv.py
@@ -0,0 +1,7 @@
+#Using this script to convert a CSV file into an HTML file so that I can use the HTML file as the data source for the Bootstrap "Responsive Tables" component
+import pandas as pd
+
+sourceCSV = "Resources/cities.csv"
+df = pd.read_csv(sourceCSV)
+
+df.to_html("Resources/cities.html")
